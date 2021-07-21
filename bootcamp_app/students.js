234dad3a68753572be7e1a6fa7e2bc8af6195ebe
Taejin Jung <taejin5314@gmail.com>
2021-07-21 03:23:54 +0000

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,3 @@
-const args = process.argv.slice(2);
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,15 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT students.id, students.name, cohorts.name AS cohort
-  FROM students
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '${'%'+args[0]+'%'}'
-  LIMIT ${args[1] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name, cohorts.name AS cohort
+    FROM students
+    JOIN cohorts ON students.cohort_id = cohorts.id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;
+`
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
     FROM teachers
     JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
     JOIN students ON students.id = assistance_requests.student_id
     JOIN cohorts ON cohorts.id = students.cohort_id
-    WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+    WHERE cohorts.name = $1
     ORDER BY teacher;
-`)
+`
+const cohortName = process.argv[2] || 'JUL02';
+
+pool.query(queryString, [cohortName])
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`)
